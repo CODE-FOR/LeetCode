81131612145d3d8fcb09b6697e2cc1a369acd7fb
CODE-FOR <meetliuzhenwei@gmail.com>
2022-10-09 08:06:46 +0000

    利用前缀和解决最大子数组和问题

--- dynamic programming/53_max_sub_array.cpp
+++ dynamic programming/53_max_sub_array.cpp
@@ -4,46 +4,46 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int maxSubArray(vector<int> &nums) {
-        vector<int> dp(nums.size() + 1, 0);
-        int max = nums[0];
-        for (int i = 0; i < nums.size(); i++) {
-            if (dp[i] > 0) {
-                dp[i + 1] = dp[i] + nums[i];
-            } else {
-                dp[i + 1] = nums[i];
-            }
-            if (dp[i + 1] > max) {
-                max = dp[i + 1];
-            } 
-        }
-        return max;
-    }    
-};
+// class Solution {
+// public:
+//     int maxSubArray(vector<int> &nums) {
+//         vector<int> dp(nums.size() + 1, 0);
+//         int max = nums[0];
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (dp[i] > 0) {
+//                 dp[i + 1] = dp[i] + nums[i];
+//             } else {
+//                 dp[i + 1] = nums[i];
+//             }
+//             if (dp[i + 1] > max) {
+//                 max = dp[i + 1];
+//             } 
+//         }
+//         return max;
+//     }    
+// };
 
 
 
 /// prefix_sum角度
-// class Solution {
-// public:
-//     int maxSubArray(vector<int> &nums) {
-//         vector<int> prefix_sum(nums.size() + 1);
-//         prefix_sum[0] = 0;
-//         for (int i = 1; i < nums.size() + 1; i++) {
-//             prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1];
-//         }
-//         int min_prefix_sum = 0;
-//         int max_sub_array = prefix_sum[1];
-//         for (int i = 1; i < nums.size() + 1; i++) {
-//             if (prefix_sum[i] - min_prefix_sum > max_sub_array) {
-//                 max_sub_array = prefix_sum[i] - min_prefix_sum;
-//             }
-//             if (prefix_sum[i] < min_prefix_sum) {
-//                 min_prefix_sum = prefix_sum[i];
-//             }
-//         }
-//         return max_sub_array;
-//     }
-// };
+class Solution {
+public:
+    int maxSubArray(vector<int> &nums) {
+        vector<int> prefix_sum(nums.size() + 1);
+        prefix_sum[0] = 0;
+        for (int i = 1; i < nums.size() + 1; i++) {
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1];
+        }
+        int min_prefix_sum = 0;
+        int max_sub_array = prefix_sum[1];
+        for (int i = 1; i < nums.size() + 1; i++) {
+            if (prefix_sum[i] - min_prefix_sum > max_sub_array) {
+                max_sub_array = prefix_sum[i] - min_prefix_sum;
+            }
+            if (prefix_sum[i] < min_prefix_sum) {
+                min_prefix_sum = prefix_sum[i];
+            }
+        }
+        return max_sub_array;
+    }
+};
