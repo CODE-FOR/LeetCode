73a7b518f284b6b5e35155df4ed1d062fd92d388
CODE-FOR <meetliuzhenwei@gmail.com>
2022-10-11 08:34:41 +0000

    利用动态规划解决交错字符串问题

---
+++ dynamic programming/97_is_inter_leave.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) {
+            return false;
+        }
+        // bool res[s1.size() + 1][s2.size() + 1];
+        vector<vector<bool>> res(s1.size() + 1, vector<bool>(s2.size() + 1));
+        res[0][0] = true;
+        if (s2.empty()) {
+            return s1 == s3;
+        }
+        if (s1.empty()) {
+            return s2 == s3;
+        }
+        for (int i = 1; i <= s1.size(); i++) {
+            res[i][0] = res[i - 1][0] & (s1[i - 1] == s3[i - 1]);
+        }
+        for (int j = 1; j <= s2.size(); j++) {
+            res[0][j] = res[0][j - 1] & (s2[j - 1] == s3[j - 1]);
+        }
+        for (int i = 1; i <= s1.size(); i++) {
+            for (int j = 1; j <= s2.size(); j++) {
+                res[i][j] = (res[i - 1][j] & (s1[i - 1] == s3[i - 1 + j])) |
+                            (res[i][j - 1] & (s2[j - 1] == s3[i + j -1]));
+            }
+        }
+        return res[s1.size()][s2.size()];
+    }
+};
