26d75921487468dbcdd6843809f28137b1d3ea2b
CODE-FOR <meetliuzhenwei@gmail.com>
2022-10-13 12:48:08 +0000

    利用贪心解决买卖股票的最佳时机II问题

---
+++ greedy/122_max_profit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        bool is_up = false;
+        bool up_start = 0;
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (!is_up && prices[i] > prices[i - 1]) {
+                is_up = true;
+                up_start = i - 1;
+            }
+            if (is_up && prices[i] < prices[i - 1]) {
+                is_up = false;
+                profit += prices[i - 1] - prices[up_start];
+            }
+            if (is_up && i + 1 == prices.size()) {
+                profit += prices[i] - prices[up_start]
+            }
+        }
+        return profit;
+    }
+};
