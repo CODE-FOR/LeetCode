cd361d028cbf084ccee6b45209fab84251058e0d
CODE-FOR <meetliuzhenwei@gmail.com>
2022-10-18 06:30:51 +0000

    fix ce

--- greedy/122_max_profit.cpp
+++ greedy/122_max_profit.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         bool is_up = false;
-        bool up_start = 0;
+        int up_start = 0;
         int profit = 0;
         for (int i = 1; i < prices.size(); i++) {
             if (!is_up && prices[i] > prices[i - 1]) {
@@ -19,7 +19,7 @@ public:
                 profit += prices[i - 1] - prices[up_start];
             }
             if (is_up && i + 1 == prices.size()) {
-                profit += prices[i] - prices[up_start]
+                profit += prices[i] - prices[up_start];
             }
         }
         return profit;
